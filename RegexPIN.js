0142526007c7234d22fd96fb54b2b91076bdf98d
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-27 15:14:10 +0530

    Added: Validating both 400 048 and 400048

--- RegexPIN.js
+++ RegexPIN.js
@@ -1,7 +1,7 @@
 //UC1
-        // Restrict the pin code when spl character added to end of it
-  let pinRegex = RegExp('^[0-9]{6}$')
-  var pinCode = 400048;
+        // validating 400 048 and 400048
+  let pinRegex = RegExp("[0-9]{3}[\\s]{0,1}[0-9]{3}")
+  var pinCode = "400048";
   if(pinRegex.test(pinCode))
     console.log(pinCode);
   else 
