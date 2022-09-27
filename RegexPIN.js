d61af86235024ea81949d07466e08d632b0096d9
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-27 15:26:02 +0530

    Added: Validating email which has abc, bridgelabz and co are mandatory

--- RegexPIN.js
+++ RegexPIN.js
@@ -1,11 +1,13 @@
-//UC1
-        // validating 400 048 and 400048
+//UC1 EMAIL
+        // validating email contains abc.xyz@bridgelabz.co.in  abc,bridgelabz and co are mandatory
   let pinRegex = RegExp("[0-9]{3}[\\s]{0,1}[0-9]{3}")
   var pinCode = "400048";
   if(pinRegex.test(pinCode))
     console.log(pinCode);
   else 
     throw 'PIN CODE is Incorrect';
+
+
 
 
     
