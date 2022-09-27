0495eb427b08f7646c0e9f21ef5f3cfeb877745b
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-27 15:02:02 +0530

    Added: Validated PIN CODE and if any spl character added to it, It should fail

--- RegexPIN.js
+++ RegexPIN.js
@@ -1,9 +1,11 @@
-
-        // validating PIN CODE
+//UC1
+        // validating PIN CODE and checking A400048 should fail
   let pinRegex = RegExp('^[0-9]{6}$')
-  var pinCode = 400048;
+  var pinCode = A400048;
   if(pinRegex.test(pinCode))
     console.log(pinCode);
   else 
     throw 'PIN CODE is Incorrect';
+
+
     
