acb271c5701c2e35464a96e3b461405bd8be67e0
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-29 09:29:21 +0530

    Added: validate email

--- RegexPIN.js
+++ RegexPIN.js
@@ -1,11 +1,11 @@
 //UC1 EMAIL
         // validating email contains abc.xyz@bridgelabz.co.in  abc,bridgelabz and co are mandatory
-  let pinRegex = RegExp("[0-9]{3}[\\s]{0,1}[0-9]{3}")
-  var pinCode = "400048";
+  let pinRegex = RegExp("[abc+](.+)?[a-z](@)?[bl+](.+)[co+](.+)[a-z]{2}$");
+  var pinCode = "abc.xyz@bl.co.in";
   if(pinRegex.test(pinCode))
     console.log(pinCode);
   else 
-    throw 'PIN CODE is Incorrect';
+    throw 'Email is Incorrect';
 
 
 
